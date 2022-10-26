66aa32863ef495291a7cdd8522c19162648b4b65
scbjt <116614261+scbjt@users.noreply.github.com>
2022-10-26 19:33:42 +0800

    Add files via upload

---
+++ 3.4百分划等级.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int grade;
+	scanf("%d",&grade);
+	if(grade>=90&&grade<=100)
+	    printf("A");
+	else 
+	if(grade>=80&&grade<=89)
+	    printf("B");
+	else
+	 if(grade>=70&&grade<=79)
+	    printf("C");
+	else
+	 if(grade>=60&&grade<=69)
+	    printf("D");
+	else
+	 if(grade>0&&grade<60)
+	    printf("E");
+    else
+    printf("ÊäÈëÊı¾İ´í");
+	return 0;
+}
